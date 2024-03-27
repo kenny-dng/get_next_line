45a75df4de8973a8694c933d87febc1c40e06025
Kenny DUONG <chikhang.duong@veepee.com>
2024-03-27 10:33:31 +0100

    reorgarnisation

---
+++ src/get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chduong <chduong@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/07 11:11:46 by chduong           #+#    #+#             */
+/*   Updated: 2021/06/22 17:17:08 by chduong          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
+size_t	ft_strlen( char *s);
+char	*save_join(char *save, char *buff);
+int		get_next_line(int fd, char **line);
+
+#endif
